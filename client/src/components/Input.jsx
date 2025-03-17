50db3a7eab9f920115e75d425235a8cec303c02f
Chandan Kanaujia <chandanegc@gmail.com>
2025-03-17 14:31:06 +0530

    complete folder and file structure of front-end

--- client/src/components/Input.jsx
+++ client/src/components/Input.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
-const App = () => {
+const Input = () => {
   return (
     <div>
-     aa 
+      
     </div>
   )
 }
 
-export default App
+export default Input
--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
-const App = () => {
+const LoginPage = () => {
   return (
     <div>
-     aa 
+      
     </div>
   )
 }
 
-export default App
+export default LoginPage
